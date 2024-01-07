81ebf7c98aee22350138778adf377f4f81a60dc4
ballmc <goreypvp@gmail.com>
2024-01-06 17:20:31 -0800

    add mixinextras

--- src/main/java/me/ballmc/ChatHeads/mixins/GuiNewChatMixin_TextRendering.java
+++ src/main/java/me/ballmc/ChatHeads/mixins/GuiNewChatMixin_TextRendering.java
@@ -6,6 +6,8 @@ import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiNewChat;
 import net.minecraft.util.ChatComponentText;
 import me.ballmc.ChatHeads.Main;
+
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,20 +16,23 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(value = GuiNewChat.class, priority = 990)
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
+@Debug(export = true)
+@Mixin(value = GuiNewChat.class, priority = -1000)
 public class GuiNewChatMixin_TextRendering {
     @Unique
     private ChatLine chatting$drawingLine = null;
 
     @Inject(method = "drawChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/ChatLine;getChatComponent()Lnet/minecraft/util/IChatComponent;"), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void captureChatLine(int updateCounter, CallbackInfo ci, int i, boolean bl, int j, int k, float f, float g, int l, int m, ChatLine chatLine, int n, double d, int o, int p, int q) {
-      // System.out.println("In captureChatLine");
+      System.out.println("In captureChatLine");
       chatting$drawingLine = chatLine;
     }
 
-    @Redirect(method = "drawChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;drawStringWithShadow(Ljava/lang/String;FFI)I", ordinal = 0))
-    private int redirectDrawString(String text, float x, float y, int color) {
+    @Redirect(method = "drawChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;drawStringWithShadow(Ljava/lang/String;FFI)I"))
+    private void redirectDrawString(String text, float x, float y, int color) {
         System.out.println("In drawStringWithShadow");
-        return Main.redirectDrawString(text, x, y, color, chatting$drawingLine);
+        Main.redirectDrawString(text, x, y, color, chatting$drawingLine);
     }
 }
